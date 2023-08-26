97013625e52186ef66439368d1b0f491cf31900a
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-08-26 18:28:10 +0300

    1st. Add Class: Field.

--- src/main/java/org/example/Field.java
+++ src/main/java/org/example/Field.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class Field {
+    private String name;
+    private String argumentType;
+    private String className;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArgumentType() {
+        return argumentType;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public Field(String name, String argumentType, String className) {
+        this.name = name;
+        this.argumentType = argumentType;
+        this.className = className;
+    }
+
+
 }
